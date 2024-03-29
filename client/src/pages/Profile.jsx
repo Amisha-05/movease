874bf9f9d5ff605cc786ab7b65c1c0780a617416
Amisha <shahiamisha809@gmail.com>
2024-03-29 13:23:53 +0530

    all done before searching part

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -149,6 +149,24 @@ dispatch(deleteUserSuccess(data));
       setShowListingsError(true);
     }
   };
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 return (
     <div className='p-3 max-w-lg mx-auto'>
    <h1 className='text-3xl font-semibold text-center my-7 '>Profile</h1>
@@ -224,14 +242,16 @@ return (
               </Link>
 
               <div className='flex flex-col item-center'>
-                <button
+                <button onClick={()=>handleListingDelete(listing._id)}
                   
                   className='text-red-700 uppercase'
                 >
                   Delete
                 </button>
                 <Link to={`/update-listing/${listing._id}`}>
-                  <button className='text-green-700 uppercase '>Edit</button>
+                  
+                  <button  className='text-green-700 uppercase '>Edit</button>
+                  
                 </Link>
               </div>
             </div>
